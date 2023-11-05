1a419ca7c9985391e3fbfa14ea390d849fcdeae1
Alan Palacios <alanpalacios1995@gmail.com>
2023-11-05 08:11:58 +0100

    Agregado responsive

--- src/componentes/ItemList/ItemList.jsx
+++ src/componentes/ItemList/ItemList.jsx
@@ -3,13 +3,14 @@ import '../Item/Item.css';
 
 const ItemList = ({ productos }) => {
     return (
-
-        <div className="itemlist__div">
+        <>
+       
 
             {productos.map((prod) => (
                 <Item key={prod.id} producto={prod} />
             ))}
-        </div>
+ 
+        </>
     );
 };
 
--- src/componentes/nav/ContainerItemList.jsx
+++ src/componentes/nav/ContainerItemList.jsx
@@ -1,25 +1,43 @@
 import { Link } from 'react-router-dom';
 import '../nav/NavbarStyle.css';
+import { BsPlaystation } from 'react-icons/bs';
+import { BsHouse } from 'react-icons/bs';
+import { BsNintendoSwitch } from 'react-icons/bs';
 
 const ListProducts = () => {
+
+
     return (
         <div className='productlist__div'>
             <Link to="/" className="btn__link">
+
+            <BsHouse className='btn__logo'/>
                 <button className='btn__list'>
                     Home
                 </button>
             </Link>
+            
             <Link to="/category/Playstation5" className="btn__link">
+
+                <BsPlaystation className='btn__logo' />
+
                 <button className='btn__list'>
                     Playstation 5
                 </button>
+
             </Link>
             <Link to="/category/Playstation4" className="btn__link">
+
+                <BsPlaystation className='btn__logo' />
+
                 <button className='btn__list'>
                     Playstation 4
                 </button>
             </Link>
             <Link to="/category/Nintendo" className="btn__link">
+
+                <BsNintendoSwitch className='btn__logo' />
+
                 <button className='btn__list'>
                     Nintendo Switch
                 </button>
